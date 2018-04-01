a15da3e14088a665892b01c7d8ef68ff8a843eaa
Ekevoo <x@ekevoo.com>
2018-04-01 18:10:19 -0400

    Added on and off scripts

---
+++ turn-on.sh
@@ -0,0 +1,9 @@
+# Settings
+DELAY_SECONDS=30
+URL='https://docs.google.com/presentation/d/e/2PACX-1vQ7LGi9WeOpcex-d2VXgQeT4pfHqd9h3YXWkDr9iReuKIIQMzPNBVZ5-J5xEh6wqvyO_aK858H4nQto/pub?start=true&loop=true&delayms='$DELAY_SECONDS'000'
+
+# Fix environment
+export DISPLAY=:0
+
+# Launch browser
+chromium-browser --kiosk --incognito "$URL" &
