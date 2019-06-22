080c4913a00cf0bda305c94c483275253320598c
Ekevoo <x@ekevoo.com>
2019-06-22 01:25:46 -0400

    added xinitrc

--- raspberry-pi/install.sh
+++ raspberry-pi/install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y firefox-esr xserver-xorg xinit git
+sudo apt install -y firefox-esr xserver-xorg xinit git xdotool
 
 cd
 git clone https://github.com/jassg-to/mural-digital.git
