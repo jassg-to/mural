7bb84fac51ddaab0352c3410e718bac520d8968f
Liz Balbuena <ekevoo@gmail.com>
2019-06-25 23:43:56 -0400

    Added cec-utils to install.sh

--- raspberry-pi/install.sh
+++ raspberry-pi/install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y firefox-esr xserver-xorg xinit git xdotool
+sudo apt install -y firefox-esr xserver-xorg xinit git xdotool cec-utils
 
 cd
 git clone https://github.com/jassg-to/mural-digital.git
