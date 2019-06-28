bc7be7b454d251bd251b3812cf3c4c4f3214703b
Ekevoo <x@ekevoo.com>
2019-06-28 10:33:44 -0400

    some pruning & renaming

--- raspberry-pi/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y firefox-esr xserver-xorg xinit git xdotool cec-utils
-
-cd
-git clone https://github.com/jassg-to/mural-digital.git
