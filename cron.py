75a0a60f8d0332bf8f12d85fb909f8f619659258
Liz Balbuena <ekevoo@gmail.com>
2019-06-28 21:23:59 -0400

    turns out it was easier to write a bit of python code

---
+++ cron.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
+import os.path
+import re
+import subprocess
+import tempfile
+
+import yaml
+
+here = os.path.split(os.path.realpath(__file__))[0]
+
+
+def main():
+    schedule = get_schedule()
+    table = '\n'.join(emit(parse(schedule)))
+    print("The new cron table is:")
+    print(table)
+    set_crontab(table)
+
+
+def get_schedule():
+    name = os.path.join(here, 'schedule.yaml')
+    file = open(name, 'r')
+    data = yaml.load(file)
+    return data
+
+
+def parse(schedule):
+    for weekday, items in schedule.items():
+        for item in items:
+
+            # Try on-off range:
+            match = re.match(r"^(\d+):(\d+)\s*-\s*(\d+):(\d+)$", item)
+            if match:
+                yield (weekday, match.group(1), match.group(2), 'on.sh')
+                yield (weekday, match.group(3), match.group(4), 'off.sh')
+                continue
+
+            # Try single event command
+            match = re.match(r"(\D.+)\s+(\d+):(\d+)", item)
+            if match:
+                yield (weekday, match.group(2), match.group(3), match.group(1))
+                continue
+
+            # Something went wrong!
+            raise ValueError("Could not understand: " + item)
+
+
+def emit(parsed_jobs):
+    for weekday, hour, minute, job in parsed_jobs:
+        full_path = os.path.join(here, job)
+        yield "%2s %2s * * %3s\t%s" % (minute, hour, weekday, full_path)
+
+
+def set_crontab(table):
+    file = tempfile.NamedTemporaryFile('w', delete=False)
+    file.write(table)
+    file.close()
+    subprocess.call(["/usr/bin/crontab", file.name])
+    os.unlink(file.name)
+
+
+if __name__ == '__main__':
+    main()
